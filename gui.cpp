210f2eb9a996faae821f2ac4a641f24b7a25a8cc
Lucas Pasquarelli <lucaspasq@icloud.com>
2022-11-15 16:29:02 -0500

    started gtk gui, will be changing

--- gui.cpp
+++ gui.cpp
@@ -5,10 +5,15 @@
 */
 
 #include<iostream>
+#include<GameKit/gtk.h>
+
+static void destroy(GtkWidget *widget, gpointer data){
+    gtk_main_quit();
+}
 
 using namespace std;
 
-int main(void)
-{
-    
+int main(int argc, char *argv[])
+{   
+    gtk_init(&argc, &argv);
 }
